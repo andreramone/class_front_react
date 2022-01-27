6c7c6eecc60cfa8c4b025a95946657f85bd6e8f7
andreramone <ramone_andre@hotmail.com>
2022-01-27 20:20:15 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Routes from "./Routes/Routes";
+
+
+function App() {
+  return (
+    <>
+      <Routes />
+    </>
+  );
+}
+
+export default App;
